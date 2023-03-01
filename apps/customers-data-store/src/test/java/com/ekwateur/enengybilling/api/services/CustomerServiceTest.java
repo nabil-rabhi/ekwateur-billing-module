b8f36e9871b7833e01d724e43b069984c8cb6001
Nabil Rabhi <nabil.rabhi@gmail.com>
2023-03-01 20:16:36 +0100

    correction des tests

--- apps/customers-data-store/src/test/java/com/ekwateur/enengybilling/api/services/CustomerServiceTest.java
+++ apps/customers-data-store/src/test/java/com/ekwateur/enengybilling/api/services/CustomerServiceTest.java
@@ -4,9 +4,12 @@ import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class CustomerServiceTest {
 
     @InjectMocks
